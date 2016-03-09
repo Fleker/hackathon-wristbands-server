194d0a00aca1d5b5d706b6c779ff045096b37afe
John Bucknam <jbuck594@gmail.com>
2016-03-09 15:07:37 -0500

    Updated Event
    Fixed auth_token issue

--- pages/views.py
+++ pages/views.py
@@ -14,7 +14,7 @@ import environ
 
 env = environ.Env()
 
-auth_token = env('auth_token')
+auth_token = env('auth_token', default="abc123")
 
 def home(request):
     return render(request, "index.html", {
@@ -36,7 +36,7 @@ def registration(request):
 
     if request.method == 'POST':
         data = JSONParser().parse(request);
-        if (auth_token != data['auth_token']):
+        if (not hasattr(data, 'auth_token') or auth_token != data['auth_token']):
             return HttpResponse(status=401)
         else:
             del data['auth_token']
@@ -61,7 +61,7 @@ def post_event(request):
 
     if request.method == 'POST':
         data = JSONParser().parse(request)
-        if (auth_token != data['auth_token']):
+        if (not hasattr(data, 'auth_token') or auth_token != data['auth_token']):
             return HttpResponse(status=401)
         else:
             del data['auth_token']
