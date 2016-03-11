67b9f9e76a2551f8d509716433a882d7c1fdcce2
John Bucknam <jbuck594@gmail.com>
2016-03-10 20:25:50 -0500

    Updated Auth_Token
    Fixed error with auth_token and invalid checking.

--- pages/views.py
+++ pages/views.py
@@ -36,7 +36,7 @@ def registration(request):
 
     if request.method == 'POST':
         data = JSONParser().parse(request);
-        if (not hasattr(data, 'auth_token') or auth_token != data['auth_token']):
+        if ((not 'auth_token' in data) or auth_token != data['auth_token']):
             return HttpResponse(status=401)
         else:
             del data['auth_token']
@@ -64,7 +64,7 @@ def post_event(request):
 
     if request.method == 'POST':
         data = JSONParser().parse(request)
-        if (not hasattr(data, 'auth_token') or auth_token != data['auth_token']):
+        if ((not 'auth_token' in data) or auth_token != data['auth_token']):
             return HttpResponse(status=401)
         else:
             del data['auth_token']
